80a2b0051bf0115d08018e67e8da29eb82571f65
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-06-03 17:21:30 -0500

    links externos

--- juan_store/orders/mails.py
+++ juan_store/orders/mails.py
@@ -1,16 +1,26 @@
 from django.conf import settings
-
+from django.urls import reverse
 from django.template.loader import get_template
 from django.core.mail import EmailMultiAlternatives
 
 class Mail:
+    @staticmethod
+    def get_absolute_url(url):
+        if settings.DEBUG:#en modo desarrollo
+            return "http://127.0.0.1:8000{}".format(
+                reverse(url)
+            )
+    
+    
+    #enviar correo
     @staticmethod
     def send_complete_order(order, user):
         subject = "tu pedido ha sido enviado" #mensaje asunto correo 
         template = get_template("orders/mails/complete.html") #template a redirigir
         content = template.render({#contexto
             "user": user,
-            "order":order
+            "order":order,
+            "next_url": Mail.get_absolute_url("orders:completeds")
 
         })
 
