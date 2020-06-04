783ed3429bb4fcd55b14738694247cbc6395e465
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-06-03 19:46:04 -0500

    Enviar codigo promocional

--- juan_store/promo_codes/views.py
+++ juan_store/promo_codes/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
+from .models import PromoCode
 # Create your views here.
 def validate(request):
+    code = request.GET.get("code")
+    promo_code = PromoCode.objects.filter(code=code).first()
+
+    if promo_code is None:
+        return JsonResponse({
+            "status":False
+    }, status=404)
     return JsonResponse({
-        "Nombre": "Juan",
-        "trabajo": "codigoFacilito",
-        "courses":[
-            {"titulo": "python"},
-            {"titulo": "HTML"},
-            {"titulo": "Base de datos"},
-        ]
+        "status":True,
+        "code": promo_code.code,
+        "discount": promo_code.discount
     })
